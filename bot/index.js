a7bafa43d6c5dbe4a5ca1ec5a5b46020ecfc8b85
Tamara <44346875+Tamaaraa@users.noreply.github.com>
2024-12-09 16:10:55 +0100

    Added attachment exclusion

--- bot/index.js
+++ bot/index.js
@@ -47,9 +47,13 @@ client.on("interactionCreate", async (interaction) => {
       attachments.push(e.attachment);
     });
 
-    messages.set(interaction.user.id, { message: interaction.targetMessage.content, attachments: attachments, user: interaction.user.globalName })
+    messages.set(interaction.user.id, {
+      message: interaction.targetMessage.content,
+      attachments: attachments,
+      user: interaction.user.globalName,
+    });
 
-    console.log(messages)
+    console.log(messages);
 
     if (attachments.size == 0) {
       await interaction.reply({
@@ -64,23 +68,26 @@ client.on("interactionCreate", async (interaction) => {
       .setCustomId("userNameInput")
       .setLabel("Contestant")
       .setStyle(TextInputStyle.Short)
-      .setPlaceholder(interaction.member.user.globalName)
+      .setValue(interaction.member.user.globalName)
       .setRequired(true);
 
     const description = new TextInputBuilder()
       .setCustomId("descriptionInput")
       .setLabel("Description")
       .setStyle(TextInputStyle.Paragraph)
-      .setPlaceholder(messages.get(interaction.user.id).message)
+      .setValue(messages.get(interaction.user.id).message)
       .setRequired(false);
 
     const excludedImages = new TextInputBuilder()
       .setCustomId("exclusionInput")
-      .setLabel(`Exclude images (1-${messages.get(interaction.user.id).attachments.length})`)
+      .setLabel(
+        `Exclude images (1-${
+          messages.get(interaction.user.id).attachments.length
+        })`
+      )
       .setStyle(TextInputStyle.Short)
-      .setPlaceholder("Keep empty to include all images, separate with comma")
+      .setPlaceholder("Empty to include all images, separate with comma")
       .setRequired(false);
-
 
     const usernameRow = new ActionRowBuilder().addComponents(username);
     const descriptionRow = new ActionRowBuilder().addComponents(description);
@@ -94,13 +101,25 @@ client.on("interactionCreate", async (interaction) => {
     interaction.customId === `modal_${interaction.user.id}`
   ) {
     const userNameInput = interaction.fields.getTextInputValue("userNameInput");
-    const descriptionInput = interaction.fields.getTextInputValue("descriptionInput")
-    const excludedImages = interaction.fields.getTextInputValue("exclusionInput")
+    const descriptionInput =
+      interaction.fields.getTextInputValue("descriptionInput");
+    const excludedImages =
+      interaction.fields.getTextInputValue("exclusionInput");
 
-    const includedAttachments = null //Exclude the numbers in excludedImages
+    excludedAttachments = new Set(
+      excludedImages.split(",").map((i) => parseInt(i) - 1)
+    );
+
+    filteredAttachments = attachments.filter(
+      (_, index) => !excludedAttachments.has(index)
+    );
 
     await interaction.reply({
-      content: `Username: ${userNameInput}\nDescription: ${descriptionInput}\nExcluded Images: ${excludedImages}\nOriginal message: ${messages.get(interaction.user.id).message}\nAttachments: \n ${messages.get(interaction.user.id).attachments.map((attachment, index) => `\t${index + 1}. ${attachment}`).join('\n')}`,
+      content: `Username: ${userNameInput}\nDescription: ${descriptionInput}\nExcluded Images: ${excludedImages}\nOriginal message: ${
+        messages.get(interaction.user.id).message
+      }\nAttachments: \n ${filteredAttachments
+        .map((attachment, index) => `\t${index + 1}. ${attachment}`)
+        .join("\n")}`,
       ephemeral: true,
     });
   }
